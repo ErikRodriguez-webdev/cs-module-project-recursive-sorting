1214358d739fba279e9810b855ebf033bb7d7d35
Erik Rodriguez <eric19.21.03@gmail.com>
2020-06-16 18:43:03 -0700

    Adds merge and merge sort function in sorting file

--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -2,27 +2,56 @@
 def merge(arrA, arrB):
     elements = len(arrA) + len(arrB)
     merged_arr = [0] * elements
-
     # Your code here
-
+    # Grab two values from list a and b with a counter from for loop
+    # if one is less than other append to end of list, while other can go to current list location
+    for i in range(len(arrB)):
+        if arrA[i] < arrB[i]:
+            merged_arr[i] = arrA[i]
+            merged_arr.append(arrB[i])
+        elif arrB[i] < arrA[i]:
+            merged_arr[i] = arrB[i]
+            merged_arr.append(arrA[i])
 
     return merged_arr
 
 # TO-DO: implement the Merge Sort function below recursively
+
+
 def merge_sort(arr):
     # Your code here
+    # if len(list) is 0, then return list
 
+    # use len(list) to find lowest, middle, and highest
+    # partition into list of 1
+    # compare neighbors and merge
+    lowest = 0
+    highest = len(arr)
+    middle = (lowest + highest) // 2
+
+    left = arr[lowest: middle]
+    right = arr[middle: highest]
+
+    if len(left) != 0 and len(right) != 0:
+        print(left)
+        print(right)
+        return merge_sort(left) + merge_sort(right)
+    elif len(left) == 0 and len(right) == 0:
+        return merge(left, right)
 
     return arr
 
-# STRETCH: implement the recursive logic for merge sort in a way that doesn't 
+# STRETCH: implement the recursive logic for merge sort in a way that doesn't
 # utilize any extra memory
-# In other words, your implementation should not allocate any additional lists 
+# In other words, your implementation should not allocate any additional lists
 # or data structures; it can only re-use the memory it was given as input
+
+
 def merge_in_place(arr, start, mid, end):
+    pass
     # Your code here
 
 
 def merge_sort_in_place(arr, l, r):
+    pass
     # Your code here
-
