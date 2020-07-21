c4a9290e0859cba965fac9e6287679cb7bddb8a3
Erik Rodriguez <eric19.21.03@gmail.com>
2020-07-21 15:15:14 -0700

    Adds code to merge on sorting file

--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -3,57 +3,58 @@ def merge(arrA, arrB):
     elements = len(arrA) + len(arrB)
     merged_arr = [0] * elements
     # Your code here
-    # Grab two values from list a and b with a counter from for loop
-    # if one is less than other append to end of list, while other can go to current list location
-    # for i in range(0, len(arrB), 2):
-    #     if arrA[i] < arrB[i]:
-    #         merged_arr[i] = arrA[i]
-    #         merged_arr[i + 1] = arrB[i]
-    #     elif arrB[i] < arrA[i]:
-    #         merged_arr[i] = arrB[i]
-    #         merged_arr[i + 1] = arrA[i]
+    # set pointer_a to zero
+    # set pointer_b to zero
+    pointer_a, pointer_b = 0, 0
 
-    # return merged_arr
+    # for i length of merged_arr loop
+    for i in range(len(merged_arr)):
+        # check if pointer_a == length of arrA
+        if pointer_a == len(arrA):
+            # then overide and set merged_arr[i] to arrB[pointer_b]
+            merged_arr[i] = arrB[pointer_b]
+            # add one to pointer_b
+            pointer_b += 1
+        # check if pointer_b == length of arrB
+        elif pointer_a == len(arrB):
+            # then overide and set merged_arr[i] to arrA[pointer_a]
+            merged_arr[i] = arrA[pointer_a]
+            # add one to pointer_a
+            pointer_a += 1
+        # check if arrA[pointer_a] > arrB[pointer_b]
+        elif arrA[pointer_a] > arrB[pointer_b]:
+            # then overide and set merged_arr[i] to arrB[pointer_b]
+            merged_arr[i] = arrB[pointer_b]
+            # add one to pointer_b
+            pointer_b += 1
+        # check if arrA[pointer_a] < arrB[pointer_b]
+        elif arrA[pointer_a] < arrB[pointer_b]:
+            # then overide and set merged_arr[i] to arrA[pointer_a]
+            merged_arr[i] = arrA[pointer_a]
+            # add one to pointer_a
+            pointer_a += 1
+
+    return merged_arr
 
 # TO-DO: implement the Merge Sort function below recursively
 
 
 def merge_sort(arr):
+    pass
     # Your code here
-    # if len(list) is 0, then return list
+    #
 
-    # use len(list) to find lowest, middle, and highest
-    # partition into list of 1
-    # compare neighbors and merge
-    # lowest = 0
-    # highest = len(arr)
-    # middle = (lowest + highest) // 2
-
-    # left = arr[lowest: middle]
-    # right = arr[middle: highest]
-
-    # if len(left) != 1 and len(right) != 1:
-    #     print(left)
-    #     print(right)
-    #     return merge_sort(left) + merge_sort(right)
-    # elif len(left) == 1 and len(right) == 1:
-    #     print(left)
-    #     print(right)
-    #     return merge(left, right)
-
-    # return arr
+    return arr
 
     # STRETCH: implement the recursive logic for merge sort in a way that doesn't
     # utilize any extra memory
     # In other words, your implementation should not allocate any additional lists
     # or data structures; it can only re-use the memory it was given as input
 
+    def merge_in_place(arr, start, mid, end):
+        pass
+        # Your code here
 
-def merge_in_place(arr, start, mid, end):
-    pass
-    # Your code here
-
-
-def merge_sort_in_place(arr, l, r):
-    pass
-    # Your code here
+    def merge_sort_in_place(arr, l, r):
+        pass
+        # Your code here
